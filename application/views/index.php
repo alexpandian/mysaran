453c5bb4c9fc4d2b8cdc578d2208d0a850086321
Alex pandian <email2alexpandian@gmail.com>
2018-02-23 17:58:39 +0530

    23/02/2018 06:00pm

--- application/views/index.php
+++ application/views/index.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>mysaran techies</title>
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./assets/favicon.ico" type="image/x-icon">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./assets/css/owl.carousel.css">
@@ -24,10 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="./assets/js/bootstrap.min.js"></script>
     <script src="./assets/js/owl.carousel.min.js"></script>
     <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/min/waypoints.min.js"></script>
-    <script src="./assets/js/jquery.counterup.js"></script>
-
-    
 
 
     <script src="./assets/js/main.js"></script>
@@ -54,18 +50,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="<?php echo base_url() ?>">
                   <img src="./assets/img/logo.png" alt="Logo">
                 </a>
               </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul class="nav navbar-nav navbar-right">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="work.html">Work</a></li>
-                    <li><a href="#">Support</a></li>
-                    <li><a href="#">Service</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="<?php echo base_url() ?>">mysaran</a></li>
+                    <li><a href="<?php echo base_url() ?>">about</a></li>
+                    <li><a href="<?php echo base_url() ?>">services</a></li>
+                    <li><a href="<?php echo base_url() ?>">careers</a></li>
+                    <li><a href="<?php echo base_url() ?>">contact</a></li>
                   </ul>
                 </div><!-- /.navbar-collapse -->
               </div><!-- /.container-fluid -->
@@ -82,7 +78,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-10 col-md-offset-2">
             <div class="block">
               <h1 class="animated fadeInUp">mysaran techies</h1>
-              <p class="animated fadeInUp">We love the Web and the work we do.We work closely with our </br> clients to deliver the best possible solutions for their needs</p>
+              <p class="animated fadeInUp">We love the Web and the work we do. We work closely with our </br> clients to deliver the best possible solutions for their needs</p>
             </div>
           </div>
         </div>
@@ -95,10 +91,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7 col-sm-12">
             <div class="block">
               <div class="section-title">
-                <h2>About Us</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+                <h2>welcome</h2>
+                <p>to mysaran techies.We love the Web and the work we do.</p>
               </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id </p>
+              <p>
+                As you know, we are into the era of digitalization. We feel bored to carry and mainthain physical documents.
+                As telecommunication becomes faster and bigger, data sharing is essential part of today's life. 
+                We help you to have your bessiness upto the best in digital era.
+              </p>
             </div>
           </div><!-- .col-md-7 close -->
           <div class="col-md-5 col-sm-12">
@@ -113,93 +113,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <section id="feature">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-md-offset-6">
-          <h2>WE BELIEVE IN GREAT IDEAS</h2>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <a href="work.html" class="btn btn-view-works">View Works</a>
+        <div id="service" class="col-md-6 col-md-offset-6">
+          <h2>SERVICES</h2>
+
+          <div class="col-sm-6 col-md-6">
+            <div class="service-item">
+              <i class="icon ion-coffee"></i>
+              <h4>Customized Software</h4>
+              <p>Get your software as like you wish, imagine. We help you to get your digital dream come true.</p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-6">
+            <div class="service-item">
+              <i class="ion-earth"></i>
+              <h4>Website</h4>
+              <p>Why not have your presence all over the world? Let anyone at anywhere, anytime come to know about your success.</p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-6">
+            <div class="service-item">
+              <i class="ion-headphone"></i>
+              <h4>Mobile App</h4>
+              <p>Have your bussiness in digital format with you always. Get to know your customers as fast as electricity.</p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-6">
+            <div class="service-item">
+              <i class="ion-leaf"></i>
+              <h4>Digital Support in All forms</h4>
+              <p>We aid you in digital world in all forms. Come to us to be not worried about your bussiness's digital success.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
         
-    <!-- Service Start -->
-    <section id="service">
-      <div class="container">
-        <div class="row">
-          <div class="section-title">
-            <h2>Our Services</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
-          </div>
-        </div>
-        <div class="row ">
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="icon ion-coffee"></i>
-              <h4>Branding</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-compass"></i>
-              <h4>Web Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-image"></i>
-              <h4>App Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-bug"></i>
-              <h4>Start Up</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-headphone"></i>
-              <h4>Logo Design</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-leaf"></i>
-              <h4>Development</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-planet"></i>
-              <h4>Brand Identity</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-3">
-            <div class="service-item">
-              <i class="ion-earth"></i>
-              <h4>Brand Identity</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <!-- Content Start -->
     <section id="testimonial">
       <div class="container">
         <div class="row">
           <div class="section-title text-center">
-            <h2>Fun Facts About Us</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+            <h2>mysaran techies</h2>
           </div>
         </div>
         <div class="row">
@@ -209,29 +164,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <li>
                   <div class="block">
                     <i class="ion-ios-chatboxes-outline"></i>
-                    <h4 class="counter">20000</h4>
-                    <span>Cups Of Coffee</span>
+                    <h4 class="counter"></h4>
+                    <span>Friendly Customer support</span>
                   </div>
                 </li>
                 <li>
                   <div class="block">
                     <i class="ion-ios-glasses-outline"></i>
-                    <h4 class="counter">20000</h4>
-                    <span>Cups Of Coffee</span>
+                    <h4 class="counter"></h4>
+                    <span>Indepth Vission</span>
                   </div>
                 </li>
                 <li>
                   <div class="block">
                     <i class="ion-ios-compose-outline"></i>
-                    <h4 class="counter">20000</h4>
-                    <span>Cups Of Coffee</span>
+                    <h4 class="counter"></h4>
+                    <span>Easily Accessible</span>
                   </div>
                 </li>
                 <li>
                   <div class="block">
                     <i class="ion-ios-timer-outline"></i>
-                    <h4 class="counter">20000</h4>
-                    <span>Cups Of Coffee</span>
+                    <h4 class="counter"></h4>
+                    <span>Timely Delivery</span>
                   </div>
                 </li>
 
@@ -245,32 +200,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <img src="./assets/img/cotation.png" alt="IMG">
                     <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
                     <div class="user">
-                      <img src="./assets/img/item-img1.jpg" alt="Pepole">
-                      <p><span>Rose Ray</span> CEO</p>
+                      <img src="./assets/img/sathish.jpg" alt="Pepole">
+                      <p><span>Sathish Kumar</span> Commercial Taxes</p>
                     </div>
                 </div>
                 <div>
                   <img src="./assets/img/cotation.png" alt="IMG">
-                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <p>"Mysan techies helping my bussiness to grow in a rapid manner. Wish them the best."</p>
                     <div class="user">
-                      <img src="./assets/img/item-img1.jpg" alt="Pepole">
-                      <p><span>Rose Ray</span> CEO</p>
+                      <img src="./assets/img/prabhu.jpg" alt="Pepole">
+                      <p><span>Prabu</span> Manager</p>
                     </div>
                 </div>
                 <div>
                   <img src="./assets/img/cotation.png" alt="IMG">
-                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <p>"Good support.Great vission. Friedly approach."</p>
                     <div class="user">
-                      <img src="./assets/img/item-img1.jpg" alt="Pepole">
-                      <p><span>Rose Ray</span> CEO</p>
-                    </div>
-                </div>
-                <div>
-                  <img src="./assets/img/cotation.png" alt="IMG">
-                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
-                    <div class="user">
-                      <img src="./assets/img/item-img1.jpg" alt="Pepole">
-                      <p><span>Rose Ray</span> CEO</p>
+                      <img src="./assets/img/mani.jpg" alt="Pepole">
+                      <p><span>Mani</span> CEO</p>
                     </div>
                 </div>
               </div>
@@ -295,11 +242,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-12">
             <div class="footer-manu">
               <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact us</a></li>
-                <li><a href="#">How it works</a></li>
-                <li><a href="#">Support</a></li>
-                <li><a href="#">Terms</a></li>
+                <li><a href="#">mysaran</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">services</a></li>
+                <li><a href="#">careers</a></li>
+                <li><a href="#">contact</a></li>
               </ul>
             </div>
           </div>
